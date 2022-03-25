9afb9ac633469a85c0cf2a3a7c2b1525d44eb7e8
Damien Mauger <mauger.damien@hotmail.fr>
2022-03-25 20:04:21 +0100

    template ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [mail, setMail] = useState({
+    name: "",
+    objet: "",
+    message: "",
+  });
+
+  const handleText = (e, id) => {
+    if (id === 1) {
+      setMail({ ...mail, name: e.target.value });
+    }
+    if (id === 2) {
+      setMail({ ...mail, objet: e.target.value });
+    }
+    if (id === 3) {
+      setMail({ ...mail, message: e.target.value });
+    }
+  };
+
+  const sendMail = () => {
+    // eslint-disable-next-line no-console
+    console.log("Hello World");
+  };
+
   return (
-    <section>
-      <p>Hello World</p>
+    <section className="global">
+      <form className="formEmail">
+        <label className="labelEmail" htmlFor="name">
+          Nom
+          <input
+            id="name"
+            name="name"
+            type="text"
+            value={mail.name}
+            onChange={(e) => handleText(e, 1)}
+          />
+        </label>
+        <label className="labelEmail" htmlFor="objet">
+          Objet
+          <input
+            id="objet"
+            name="objet"
+            type="text"
+            value={mail.objet}
+            onChange={(e) => handleText(e, 2)}
+          />
+        </label>
+        <label className="labelEmail" htmlFor="message">
+          Message
+          <textarea
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+            value={mail.message}
+            onChange={(e) => handleText(e, 3)}
+          />
+        </label>
+        <button type="button" onClick={sendMail}>
+          Envoyer
+        </button>
+      </form>
     </section>
   );
 }
