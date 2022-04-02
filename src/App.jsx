0a797dc0381c0124891b8ce62f4f516c547ed49b
Damien Mauger <mauger.damien@hotmail.fr>
2022-04-02 03:35:19 +0200

    finish test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-one-expression-per-line */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
 import "./App.scss";
 
 function App() {
@@ -8,16 +9,38 @@ function App() {
   const [newMessage, setNewMessage] = useState("");
   const [socket, setSocket] = useState(null);
 
+  useEffect(() => {
+    const socket = socketIOClient("http://localhost:8000");
+    setSocket(socket);
+
+    socket.on("initialMessage", (initialMessage) => {
+      setMessages(initialMessage);
+    });
+
+    socket.on("updateMessageList", (listMessage) => {
+      setMessages(listMessage);
+    });
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    socket.emit("newMessageFromClient", {
+      author: nickname,
+      text: newMessage,
+    });
+    setNewMessage("");
   };
+
+  // Enlever le backstick après message.text
+  // changer la value du second input
+  // puis push le nouveau template
 
   return (
     <section>
       <h2>Messages</h2>
       {messages?.map((message) => (
         <div key={message.id}>
-          {message.author} : {message.text}`
+          {message.author} : {message.text}
         </div>
       ))}
       <h2>New message</h2>
@@ -34,7 +57,7 @@ function App() {
           type="text"
           name="messageContent"
           placeholder="Message"
-          value={nickname}
+          value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
           required
         />
